9d19d2f539101c11d928787fcf37fe0c789e5eec
Alberto Pirillo <alberto.pirillo@mail.polimi.it>
2022-10-27 12:14:46 +0200

    Add new script to remove references to deleted entries

---
+++ scripts/deprecated_references.py
@@ -0,0 +1,25 @@
+"""
+    This script checks if the values in the "references" field of the dataset are valid.
+    If a reference points to a paper present in the dataset, it considered valid and kept. Otherwise, it is removed.
+    This check is necessary since we are using only a subset of the initial dataset.
+"""
+
+import pandas as pd
+from ast import literal_eval
+
+in_file_path = ""
+out_file_path = ""
+
+if __name__ == "__main__":
+    # Load the dataset
+    df = pd.read_csv(in_file_path, encoding="UTF-8")
+
+    # Remove references to non-existing ids
+    ref_list_list = df["references"].apply(literal_eval).to_numpy()
+    present_ids = set(df["id"])
+    df["references"] = [[ref for ref in ref_list if int(ref) in present_ids] for ref_list in ref_list_list]
+
+    # Write the updated dataset
+    df.to_csv(out_file_path, encoding="UTF-8", index=False, header=True, escapechar="|")
+
+    print(f"Operation complete.")
