19b2b73f265e2562fba6125878332748f569cc01
Alberto Pirillo <alberto.pirillo@mail.polimi.it>
2022-10-29 12:27:07 +0200

    Edit scipts to split the dataset into multiple datasets

--- scripts/dataset_preprocessing.py
+++ scripts/dataset_preprocessing.py
@@ -1,30 +1,72 @@
 """
-  In this script the whole dataset is cleaned, reduced and converted to the CSV format.
+  In this script the whole dataset is cleaned and reduced in size.
+  The initial dataset is also split into multiple separate datasets.
   If the script crashes due to memory errors, lower the chunk_size and try again.
 """
 
-# Uncomment if using Colab and if the dataset is stored on Google Drive
-# from google.colab import drive
-# drive.mount('/content/drive')
-
-import pandas as pd
 import numpy as np
+import pandas as pd
+import functools
+from multiprocessing import Pool
 
 # Config
-file_path = ""
-out_file_path = ""
+in_file_path = ""
+out_folder_path = ""
 chunk_size = 100_000
-citation_thresh = 10
-references_thresh = 10
+citation_thresh = 100
+references_thresh = 30
 
-# Structure of the columns of the dataset
-col_labels = ("id", "title", "authors", "venue", "year", "n_citation", "page_start", "page_end", "doc_type",
-              "publisher", "volume", "issue", "fos", "doi", "references", "abstract")
+# Structure of the columns of the paper of the dataset
+col_labels = ("id", "title", "year", "n_citation", "page_start", "page_end", "doc_type",
+              "publisher", "volume", "issue", "doi", "references", "abstract")
+
+
+def author_dataset(dataset, header):
+    # Create the author dataset
+    author_df = pd.DataFrame(columns=("paper_id", "author_name", "author_id", "author_org"))
+    for index, row in dataset[["id", "authors"]].iterrows():
+        for author in row["authors"]:
+            new_row = {"paper_id": row["id"], "author_name": author.get("name"),
+                       "author_id": author.get("id"), "author_org": author.get("org")}
+            author_df = pd.concat([author_df, pd.DataFrame([new_row])], ignore_index=True)
+    # Store the author dataset in a file
+    author_df.to_csv(out_folder_path + "/author_dataset.csv", encoding="UTF-8", mode="a",
+                     index=False, header=header, escapechar="|")
+
+
+def venue_dataset(dataframe, header):
+    # Create the venue dataset
+    venue_df = pd.DataFrame(columns=("paper_id", "venue_name", "venue_id"))
+    for index, row in dataframe[["id", "venue"]].iterrows():
+        venue = row["venue"]
+        new_row = {"paper_id": row["id"], "venue_name": venue.get("raw"), "venue_id": venue.get("id")}
+        venue_df = pd.concat([venue_df, pd.DataFrame([new_row])], ignore_index=True)
+    # Store the venue dataset in a file
+    venue_df.to_csv(out_folder_path + "/venue_dataset.csv", encoding="UTF-8", mode="a",
+                    index=False, header=header, escapechar="|")
+
+
+def fos_dataset(dataset, header):
+    # Create the fos dataset
+    fos_df = pd.DataFrame(columns=("paper_id", "fos_name", "fos_weight"))
+    for index, row in dataset[["id", "fos"]].iterrows():
+        for fos in row["fos"]:
+            new_row = {"paper_id": row["id"], "fos_name": fos.get("name"), "fos_weight": fos.get("w")}
+            fos_df = pd.concat([fos_df, pd.DataFrame([new_row])], ignore_index=True)
+    # Store the venue dataset in a file
+    fos_df.to_csv(out_folder_path + "/fos_dataset.csv", encoding="UTF-8", mode="a",
+                  index=False, header=header, escapechar="|")
+
+
+def smap(f):
+    return f()
+
 
 if __name__ == "__main__":
     df = pd.DataFrame()
+    is_first_chunk = True
 
-    with pd.read_json(file_path, encoding="UTF-8", lines=True, chunksize=chunk_size) as reader:
+    with pd.read_json(in_file_path, encoding="UTF-8", lines=True, chunksize=chunk_size) as reader:
         block_count = 0
         for chunk in reader:
             block_count += chunk_size
@@ -63,12 +105,18 @@ if __name__ == "__main__":
                 indexed_abstract[i] = " ".join(string_arr)
             df = df.rename({"indexed_abstract": "abstract"}, axis=1)
 
-            # Store the chunk in the .csv file
-            if block_count == chunk_size:
-                df.to_csv(out_file_path, encoding="UTF-8", index=False, mode="a",
-                          header=True, escapechar="|", columns=col_labels)
-            else:
-                df.to_csv(out_file_path, encoding="UTF-8", index=False, mode="a",
-                          header=False, escapechar="|", columns=col_labels)
+            # Perform the splitting with different threads
+            with Pool() as pool:
+                pool.map(smap, [functools.partial(author_dataset, df, is_first_chunk),
+                                functools.partial(venue_dataset, df, is_first_chunk),
+                                functools.partial(fos_dataset, df, is_first_chunk)])
+
+            # Create the paper dataset, by editing the initial one
+            df.drop(["authors", "venue", "fos"], axis=1)
+            # Store the paper dataset in a file
+            df.to_csv(out_folder_path + "/paper_dataset.csv", encoding="UTF-8", mode="a",
+                      index=False, header=is_first_chunk, escapechar="|", columns=col_labels)
+
+            is_first_chunk = False
 
     print("Conversion complete.")
--- scripts/deprecated_references.py
+++ scripts/deprecated_references.py
@@ -2,17 +2,18 @@
     This script checks if the values in the "references" field of the dataset are valid.
     If a reference points to a paper present in the dataset, it considered valid and kept. Otherwise, it is removed.
     This check is necessary since we are using only a subset of the initial dataset.
+    The update happens in-place.
 """
 
 import pandas as pd
 from ast import literal_eval
 
-in_file_path = ""
-out_file_path = ""
+# Config
+file_path = ""
 
 if __name__ == "__main__":
     # Load the dataset
-    df = pd.read_csv(in_file_path, encoding="UTF-8")
+    df = pd.read_csv(file_path, encoding="UTF-8")
 
     # Remove references to non-existing ids
     ref_list_list = df["references"].apply(literal_eval).to_numpy()
@@ -20,6 +21,6 @@ if __name__ == "__main__":
     df["references"] = [[ref for ref in ref_list if int(ref) in present_ids] for ref_list in ref_list_list]
 
     # Write the updated dataset
-    df.to_csv(out_file_path, encoding="UTF-8", index=False, header=True, escapechar="|")
+    df.to_csv(file_path, encoding="UTF-8", index=False, header=True, escapechar="|")
 
     print(f"Operation complete.")
